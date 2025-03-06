3216e2b622187a87780bd72ece3e85d4671d36e8
nb-2002 <nbbbyambaa@gmail.com>
2025-03-06 22:22:21 +0900

    Game 2048 base recommit

--- src/components/BigBox.vue
+++ src/components/BigBox.vue
@@ -2,7 +2,7 @@
     <div class="row items-center justify-evenly">
         <svg width="610" height="610">
             <rect width="610" height="610" rx="10" fill="#998C7E" />
-            <box-array :boxes="boxes"/>
+            <box-array :boxarray="boxes"/>
         </svg>
     </div>
 </template>
@@ -13,6 +13,6 @@ import {ref} from "vue";
 import type {Box} from "./structure";
 import BoxArray from "./BoxArray.vue";
 
-const boxes = ref<Box[]>([...Array(16).keys()].map(i => ({id:i, x:10+150*(i/4|0), y:10+150*(i%4), n:0, color:""})))
+const boxes = ref<Box[]>([...Array(16).keys()].map(i => ({id:i, x:10+150*(i/4|0), y:10+150*(i%4), n:0, color:"#BAAD9A"})))
 console.log("hello",boxes);
 </script>
--- src/components/BoxArray.vue
+++ src/components/BoxArray.vue
@@ -1,17 +1,17 @@
 <template>
-    <rect v-for="box in boxes" :key="box.id" width="140" height="140" rx="10" :x="box.x" :y="box.y" :fill="box.color"/>
+    <rect v-for="box in boxarray" :key="box.id" width="140" height="140" rx="10" :x="box.x" :y="box.y" :fill="box.color"/>
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
+// import {ref} from "vue";
 import type {Box} from "./structure";
 
 interface Props{
-    boxes: Box[];
+    boxarray: Box[];
 }
 
-const props = withDefaults(defineProps<Props>(), {
-    boxes: () => []
-})
+withDefaults(defineProps<Props>(), {
+    boxarray: () => []
+});
 
 </script>
